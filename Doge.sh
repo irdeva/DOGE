1eff0ed8debbe4b4a19af04b50a3c26dfec4dba4
irdeva <86414235+irdeva@users.noreply.github.com>
2021-06-26 10:34:56 +0700

    Create Doge.sh

---
+++ Doge.sh
@@ -0,0 +1,7 @@
+#DogeCoin
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz 
+tar -xvzf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig --url rx.unmineable.com:3333 --user DOGE:DELdkDT6aMfoTihgd768xgvjWSLw3ipAtv.GT#JRZWZUKX --pass x
+
